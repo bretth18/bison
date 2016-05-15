b8eaf8979846763214eb5199610ca689d90fa460
Brett Henderson <bretth18@gmail.com>
2016-05-14 22:41:36 -0700

    gimme dat time precision boi

--- Components/ListComment.js
+++ Components/ListComment.js
@@ -1,12 +1,8 @@
 import React, {
-  Linking,
-  Platform,
-  ActionSheetIOS,
-  Dimensions,
   View,
   Text,
-  Navigator,
   Component,
+  Alert,
   TouchableHighlight} from 'react-native';
 import moment from 'moment';
 const styles = require('../Styles/Styles.js');
@@ -15,23 +11,24 @@ const constants = styles.constants;
 class ListComment extends Component {
   // handle time precision
   handleTime(){
-    if (this.props.comment.time === null){
+    if (this.props.comment.time === undefined){
       return console.log('time empty');
     }
-    else if (this.props.comment.time != null){
-      var displayTime = moment(this.props.comment.time).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    else if (this.props.comment.time !== undefined){
+      return moment(this.props.comment.time).format('dddd, MMMM Do YYYY, h:mm:ss a');
     } else {
       console.log('neither cases pass');
+      Alert.alert('fuck');
     }
   }
   render(){
-    var displayTime = moment(this.props.comment.time).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    // var displayTime = moment(this.props.comment.time).format('dddd, MMMM Do YYYY, h:mm:ss a');
     return(
       <TouchableHighlight onPress={this.props.onPress}>
         <View style={styles.li}>
           <Text style={styles.liText}>{this.props.comment.comment}</Text>
           <Text style={styles.liText}>{this.props.comment.id}</Text>
-          <Text style={styles.liText}>{displayTime}</Text>
+          <Text style={styles.liText}>{this.handleTime()}</Text>
         </View>
       </TouchableHighlight>
     );

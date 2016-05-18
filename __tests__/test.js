78ef2b7a8af88e8d205c8afd6ead7c6617041506
Brett Henderson <bretth18@gmail.com>
2016-05-18 16:01:36 -0700

    jest CI tests

---
+++ __tests__/test.js
@@ -0,0 +1,12 @@
+'use strict';
+describe('test', () => {
+
+// temp test please replace
+    it('does nothing', () => {
+        const result = 'test';
+        expect(result).toBe('test');
+    });
+
+
+
+});
